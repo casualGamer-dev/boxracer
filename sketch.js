122134a6bef5ebf79c98238a4f8a7a4ece32df60
casualGamer-dev <niladridas218@gmail.com>
2020-04-02 16:47:06 +0530

    a box racer game

--- sketch.js
+++ sketch.js
@@ -4,12 +4,13 @@ var gameState = 0;
 var playerCount;
 
 var database;
-
+var car1,car2,car3,car4,cars
 var form, player, game;
 
+var allPlayers ;
 
 function setup(){
-  canvas = createCanvas(400,400);
+  canvas = createCanvas(displayWidth-20,displayHeight-30);
   database = firebase.database();
   game = new Game();
   game.getState();
@@ -18,4 +19,13 @@ function setup(){
 
 
 function draw(){
+  if(playerCount===4){
+game.update(1);
+
+  }
+  if(gameState===1){
+    clear()
+    game.play();
+console.log(gameState);
+  }
 }
